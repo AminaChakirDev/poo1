c4093c8daf1bcf32280e9f6a66c4c121184a94cb
AminaChakirDev <amina.aitm@gmail.com>
2020-04-14 21:26:50 +0200

    Refonte du contenu pour repondre aux consignes

--- Truck.php
+++ Truck.php
@@ -20,55 +20,48 @@ class Truck extends Vehicle
     private $loading;
 
 
-// Do not modify the rest of the file
 
     public function __construct(int $volumeCapacity, string $color, int $nbSeats, string $energy)
     {
         parent::__construct($color, $nbSeats);
         $this->energy = $energy;
+        $this->volumeCapacity = $volumeCapacity;
     }
 
-    public function getEnergy(): string
+    public function setLoading($loading): void
     {
-        return $this->energy;
-    }
-
-    public function setEnergy(string $energy): void
-    {
-        $this->energy = $energy;
-    }
-
-
-    public function getEnergyLevel(): int
-    {
-        return $this->energyLevel;
-    }
-
-    public function setEnergyLevel(int $energyLevel): void
-    {
-        $this->energyLevel = $energyLevel;
-    }
-
-    public function forward(): string
-    {
-        $this->loading = 15;
-        return "in filling !";
-    }
-
-    public function fill(): string
-    {
-        $sentence = "";
-        while ($this->loading < 90) {
-            $this->loading+=15;
-            $sentence .= "in filling!";
-        }
-
-        $sentence .= "full";
-        return $sentence;
+        $this->loading = $loading;
     }
 
     public function getLoading(): int
     {
         return $this->loading;
+    }
+
+    public function setVolumeCapacity(int $VolumeCapacity): void
+    {
+        $this->VolumeCapacity = $VolumeCapacity;
+    }
+
+    //public function checkVolume(): string
+    //{
+     //  if ($this->loading < $this->volumeCapacity) {
+    //        return "in filling!";
+   //     }
+   //     else {
+    //        return "full";
+    //    }
+   // }
+
+    public function fill(): string
+    {
+        $sentence = "";
+        while ($this->loading < $this->volumeCapacity) {
+            $this->loading+=15;
+            $sentence .= "In filling<br>";
+        }
+
+        $sentence .= "FULL !";
+        return $sentence;
     }
 }
--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 
 require_once ("Bicycle.php");
 
-$bike = new Bicycle("blue", 1);
+//$bike = new Bicycle("blue", 1);
 
 //var_dump($bike);
 
 
 
 
-require_once ("Car.php");
+//require_once ("Car.php");
 
 //INSTANCIATION D'UN OBJET
-$suv = new Car("black", 4, "gazoil");
+//$suv = new Car("black", 4, "gazoil");
 //var_dump($suv);
 
 //echo $suv->forward();
@@ -21,11 +21,11 @@ $suv = new Car("black", 4, "gazoil");
 //echo '<br> Vitesse du suv : ' . $suv->getCurrentSpeed() . ' km/h' . '<br>';
 //echo $suv->brake();
 
-$bicycle = new Bicycle('blue', 1);
+//$bicycle = new Bicycle('blue', 1);
 //echo $bicycle->forward();
 //var_dump($bicycle);
 
-$car = new Car('green', 4, 'electric');
+//$car = new Car('green', 4, 'electric');
 //echo $car->forward();
 //var_dump($car);
 
@@ -34,10 +34,20 @@ require_once ("Truck.php");
 
 //INSTANCIATION D'UN OBJET
 $renault = new Truck(90,"black",3,"gazoil");
-//var_dump($suv);
+var_dump($renault);
 
-echo $renault->forward();
-echo '<br> Volume du chargement : ' . $renault->getLoading() . ' Volume' . '<br>';
+$renault->setLoading(0);
+
+//PREMIERE METHODE : AUGMENTATION AUTOMATIQUE DU CHARGEMENT (VIA LA METHODE FILL)
 echo $renault->fill();
-echo '<br> Volume du chargement : ' . $renault->getLoading() . ' Volume' . '<br>';
-echo $renault->fill();
+
+//DEUXIEME METHODE : AUGMENTATION MANUELLE DU CHARGEMENT (VIA LA METHODE SETLOADING)
+//echo '<br> Volume du chargement : ' . $renault->getLoading() . ' Tonnes' . '<br>';
+//echo $renault->checkVolume();
+//$renault->setLoading(45);
+//echo '<br> Volume du chargement : ' . $renault->getLoading() . ' Tonnes' . '<br>';
+//echo $renault->checkVolume();
+//$renault->setLoading(90);
+//echo '<br> Volume du chargement : ' . $renault->getLoading() . ' Tonnes' . '<br>';
+//echo $renault->checkVolume();
+
